0cf2e2372dd7832abe267ae2f665b012572922b5
marcobo <marcobrugo@mac.com>
2019-02-25 22:14:31 -0500

    pushing finished HW

--- LIRI Code/key.js
+++ LIRI Code/key.js
@@ -1,9 +1,6 @@
 console.log('LOADED');
-
 var clientID = 'cc47aff5008c4a9db5c4933ab20e81c4';
 var clientSec = '1e648c29b8f34ca7a4e3541d80aff7a4';
-
-
 exports.spotify = {
     id: clientID,
     secret: clientSec
